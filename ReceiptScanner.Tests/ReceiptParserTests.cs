de13568cd1011a85fad157168654e732ab2c7143
Christian Genne <christian.genne@wint.se>
2014-05-10 19:07:15 +0200

    Parse fixes

--- ReceiptScanner.Tests/ReceiptParserTests.cs
+++ ReceiptScanner.Tests/ReceiptParserTests.cs
@@ -85,7 +85,51 @@ medtag aiitid vara och kvitto.
 
             var receipt = ReceiptParser.ParseReceiptText(parsedText);
             Assert.AreEqual((decimal)902.12, receipt.Total);
-            Assert.AreEqual(2, receipt.Items.First().Quantity);
+
+            var expected = new Tuple<string, double, double>[]
+            {
+                Tuple.Create("avooado", 2.0, 15.80),
+                Tuple.Create("Biodgrapejuioe", 1.0, 15.90),
+                Tuple.Create("Bärkasse papper", 1.0, 2.0),
+                Tuple.Create("Barkasae piast", 2.0, 3.0),
+                Tuple.Create("Creme Fraioha 34%", 1.0, 21.70),
+                Tuple.Create("Faiukorv gourmet", 1.0, 31.90),
+                Tuple.Create("Gourmaskinka rökt", 1.0, 31.90),
+                Tuple.Create("Grekisk saiiadsost", 1.0, 18.90),
+                Tuple.Create("Gurka ävensk", 0.39, 7.76),
+                Tuple.Create("Haiioumi", 1.0, 27.90),
+                Tuple.Create("Havretrippei", 1.0, 22.90),
+                Tuple.Create("ICA Hush Matåkök", 1.0, 26.90),
+                Tuple.Create("jordgubbar", 2.0, 20.00),
+                Tuple.Create("Kyckling", 1.0, 27.90),
+                Tuple.Create("*Kyckiingfiié", 1.0, 59.90),
+                Tuple.Create("Laxfiie 4-p ICA", 1.0, 95.00),
+                Tuple.Create("Miid yogh nat EKO", 2.0, 33.60),
+                Tuple.Create("Paprika Röd", 0.26, 7.77),
+                Tuple.Create("Potatis fast", 1.22, 10.86),
+                Tuple.Create("Russin", 1.0, 29.90),
+                Tuple.Create("Saiiads/Matbar", 0.87, 95.27),
+                Tuple.Create("Satsumas", 0.655, 19.58),
+                Tuple.Create("Sensod Tandkr 75m1", 1.0, 26.90),
+                Tuple.Create("Shamp normait gul", 1.0, 31.90),
+                Tuple.Create("Smoothie Yogh Apri", 2.0, 23.80),
+                Tuple.Create("Smoothie Yogh Jgb", 3.0, 35.70),
+                Tuple.Create("tomat körsbär", 1.0, 8.90),
+                Tuple.Create("Torsk", 1.0, 59.00),
+                Tuple.Create("V6 Duai 0oeanMint", 2.0, 33.80),
+                Tuple.Create("Viapgrädde 36%", 1.0, 20.70),
+                Tuple.Create("Äppie Granny ICA", 0.56, 10.58),
+                Tuple.Create("Äsens L.mjöik G.da", 1.0, 24.50)
+            };
+
+            var i = 0;
+            foreach(var receiptItem in receipt.Items)
+            {
+                Assert.AreEqual(expected[i].Item1, receiptItem.Name);
+                Assert.AreEqual((decimal)expected[i].Item2, receiptItem.Quantity);
+                Assert.AreEqual((decimal)expected[i].Item3, receiptItem.Price);
+                i++;
+            }
         }
 
         [TestMethod]
